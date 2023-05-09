9269ecda26e0c77e5c7e4a28b36c023db3192696
sanimations <smartucci157@gmail.com>
2023-05-09 01:04:01 -0400

    Added the pokemonList array and filled it with pokemon

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+let pokemonList = [
+    {name : 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Ditto', height: 0.3, type: 'normal'},
+    {name: 'Eevee', height: 0.3, type: 'normal'},
+    {name: 'Ekans', height: 2.0, type: 'poison'},
+    {name: 'Regigigas', height: 3.7, type: 'normal'},
+    {name: 'Suicune', height: 2.0, type: 'water'},
+    {name: 'Arcanine', height: 1.9, type: 'fire'},
+    {name: 'Cyndaquil', height: 0.5, type: 'fire'},
+    {name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy']},
+    {name: 'Poliwrath', height:1.3, type: ['water', 'fighter']},
+    {name: 'Banette', height: 1.1, type: 'ghost'}
+];
