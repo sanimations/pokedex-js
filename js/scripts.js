2b00dc8cd6cd3d54b059e0ba860b75dad9e7ab9e
sanimations <smartucci157@gmail.com>
2023-05-09 17:56:55 -0400

    Added comments to the array and for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//An array of pokemon, their names, height, and their type (or types)
 let pokemonList = [
     {name : 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
     {name: 'Ditto', height: 0.3, type: 'normal'},
@@ -11,7 +12,11 @@ let pokemonList = [
     {name: 'Poliwrath', height:1.3, type: ['water', 'fighter']},
     {name: 'Banette', height: 1.1, type: 'ghost'}
 ];
-
+/*This goes through each array cell
+it will print the name and height of the pokemon
+it contains an if statement that will also add an extra sentence
+depending on the pokemon height
+*/
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.0) {
         document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s big! ');
