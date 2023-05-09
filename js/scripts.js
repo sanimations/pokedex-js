b5ead84f307507f6c496b0ed96a719174687cd5f
sanimations <smartucci157@gmail.com>
2023-05-09 17:44:40 -0400

    added if statement for different heights

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,11 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.0) {
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s big! ');
+    } else if (pokemonList[i].height <= 0.5) {
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s tiny! ');
+    } else{
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') ');
+    }
 }
