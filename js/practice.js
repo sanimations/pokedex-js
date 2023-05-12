e2949c7b4fcb6c2181d1486cf99957ed44c12935
sanimations <smartucci157@gmail.com>
2023-05-12 02:15:00 -0400

    Moved the for loop over to the practice.js file

--- js/practice.js
+++ js/practice.js
@@ -10,3 +10,19 @@ let doubleSize = size * 2;
 let minSize = (doubleSize * 2) - (size / 2);
 document.write(minSize); 
 */
+
+/*This goes through each array cell
+it will print the name and height of the pokemon
+it contains an if statement that will also add an extra sentence
+depending on the pokemon height
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.0) {
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s big! ');
+    } else if (pokemonList[i].height <= 0.5) {
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s tiny! ');
+    } else{
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') ');
+    }
+}
+*/
--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,5 @@ let pokemonList = [
     {name: 'Poliwrath', height:1.3, type: ['water', 'fighter']},
     {name: 'Banette', height: 1.1, type: 'ghost'}
 ];
-/*This goes through each array cell
-it will print the name and height of the pokemon
-it contains an if statement that will also add an extra sentence
-depending on the pokemon height
-*/
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.0) {
-        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s big! ');
-    } else if (pokemonList[i].height <= 0.5) {
-        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that\'s tiny! ');
     } else{
-        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') ');
     }
-}
