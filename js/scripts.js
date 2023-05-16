fcc2254fc02256f8a0833a3ea60df5c37a63191b
sanimations <smartucci157@gmail.com>
2023-05-15 22:37:32 -0400

    Added a showDetails button
    currently doesn't do anything

--- js/scripts.js
+++ js/scripts.js
@@ -26,15 +26,20 @@ let pokemonRepository = (function () {
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
-        button.classList.add('pokeClass');
+        button.classList.add('pokeClass');  //creates pokeClass to the list items
         pokemonList.appendChild(listItem);  //listItem is child of pokemonList
-        listItem.appendChild(button);  //The newly created button is a CHILD of the LIST!!
+        listItem.appendChild(button);       //The newly created button is a CHILD of the LIST!!
+        button.addEventListener('click',showDetails(pokemon));    //adds event to button, on click,that shows the details of the pokemon clicked
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
     }
 
     return {
         add: add,
         getAll: getAll,
-        addListItem: addListItem
+        addListItem: addListItem,
+        showDetails: showDetails
     }
 })();
 
