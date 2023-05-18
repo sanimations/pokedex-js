853e4ed4976fa250fa8340b74726ea71fda0961b
sanimations <smartucci157@gmail.com>
2023-05-18 17:28:56 -0400

    Moved unused code to Practice file

--- js/practice.js
+++ js/practice.js
@@ -35,3 +35,31 @@ for (let i = 0; i < pokemonList.length; i++) {
         document.write(pokemon.name + ' (height:' + pokemon.height + ') ');
     }
 }) */
+
+/* fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+  return response.json(); // This returns a promise!
+}).then(function (pokemonList) {
+  console.log(pokemonList); // The actual JSON response
+}).catch(function () {
+  // Error
+}); */
+
+// { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+        // { name: 'Ditto', height: 0.3, type: 'normal' },
+        // { name: 'Eevee', height: 0.3, type: 'normal' },
+        // { name: 'Ekans', height: 2.0, type: 'poison' },
+        // { name: 'Regigigas', height: 3.7, type: 'normal' },
+        // { name: 'Suicune', height: 2.0, type: 'water' },
+        // { name: 'Arcanine', height: 1.9, type: 'fire' },
+        // { name: 'Cyndaquil', height: 0.5, type: 'fire' },
+        // { name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy'] },
+        // { name: 'Poliwrath', height: 1.3, type: ['water', 'fighter'] },
+        // { name: 'Banette', height: 1.1, type: 'ghost' }
+
+            //     pokemon.get(url, "pokemon.name" + "pokemon.type" + "pokemon.height");
+    //     let myPokemon = JSON.stringify(pokemon);
+    //     .then(funtion(json){
+    //         json.
+    //     })
+    // }
+        
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,5 @@
 //An array of pokemon, their names, height, and their type (or types)
 let pokemonRepository = (function () {
-    let pokemonList = [
-        { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
-        { name: 'Ditto', height: 0.3, type: 'normal' },
-        { name: 'Eevee', height: 0.3, type: 'normal' },
-        { name: 'Ekans', height: 2.0, type: 'poison' },
-        { name: 'Regigigas', height: 3.7, type: 'normal' },
-        { name: 'Suicune', height: 2.0, type: 'water' },
-        { name: 'Arcanine', height: 1.9, type: 'fire' },
-        { name: 'Cyndaquil', height: 0.5, type: 'fire' },
-        { name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy'] },
-        { name: 'Poliwrath', height: 1.3, type: ['water', 'fighter'] },
-        { name: 'Banette', height: 1.1, type: 'ghost' }
-    ];
 
     function add(pokemon) {
         pokemonList.push(pokemon);
