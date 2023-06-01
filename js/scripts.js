980c3a17e022ed55685800b4dbf30570a6f2dd24
sanimations <smartucci157@gmail.com>
2023-06-01 03:32:21 -0400

    tried fixing the types, but still malfunctioning

--- js/scripts.js
+++ js/scripts.js
@@ -10,76 +10,81 @@ let pokemonRepository = (function () {
     function getAll() {
         return pokemonList;
     }
-    function LoadList(){
+    function LoadList() {
 
         /*Created a promise function.  Run the fetch function on the apiUrl.
         This is the promise and the response being another promise */
-        return fetch(apiUrl).then(function(response){
-           
+        return fetch(apiUrl).then(function (response) {
+
             //the reponse is converted to json format and returned
             return response.json();
-        }).then(function(json){
+        }).then(function (json) {
 
             //Run forEach through all the API and create a pokemon object with name & details
             //json.results refers to the api in the link, results is the name of the object
-            json.results.forEach(function(item){
+            json.results.forEach(function (item) {
                 let pokemon = {
                     name: item.name,
                     detailsUrl: item.url
                 };
                 add(pokemon);  //this is the add functio we created
-                });
-            }).catch(function (e){
-                console.error(e);
-            })
-        }
-    function loadDetails(item){
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+    function loadDetails(item) {
         let url = item.detailsUrl;
-        return fetch(url).then(function (response){
+        return fetch(url).then(function (response) {
             return response.json();
 
             //This one takes the parameter details and goes into each details url in the api
             //first you find the sprite in the pokemon's details, then height, then types
-        }).then(function(details){
+        }).then(function (details) {
+            let types;
             item.frontSprite = details.sprites.front_default;
             item.height = details.height;
-            item.types = JSON.stringify(details.types);
+            //loop through types since it is an array of objects
+            details.types.forEach(function (item) {
+                types += '${item.type.name}, ';
+            });
+            details.types = types;
             item.backSprite = details.sprites.back_default;
-        }).catch(function(e){
+        }).catch(function (e) {
             console.error(e);
         });
     }
-    
+
     function addListItem(pokemon) {
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
         listItem.classList.add('list-group-item');
-        button.classList.add('pokeClass', 'btn', "col-4");  //creates pokeClass and btn to the list items
+        button.classList.add('pokeClass', 'btn');  //creates pokeClass and btn to the list items
         button.setAttribute("data-toggle", "modal")
         button.setAttribute("data-target", "#exampleModal")
         pokemonList.appendChild(listItem);  //listItem is child of pokemonList
         listItem.appendChild(button);       //The newly created button is a CHILD of the LIST!!
-        button.addEventListener('click',() => showDetails(pokemon));    //adds event to button, on click,that shows the details of the pokemon clicked
+        button.addEventListener('click', () => showDetails(pokemon));    //adds event to button, on click,that shows the details of the pokemon clicked
     }
-    
-    function showDetails(pokemon){
-        pokemonRepository.loadDetails(pokemon).then(function(){
+
+    function showDetails(pokemon) {
+        pokemonRepository.loadDetails(pokemon).then(function () {
             showModal(pokemon);
         });
     }
-    function showModal(pokemon){
+    function showModal(pokemon) {
         //saving each element inside different variable
         let modalBody = $(".modal-body");
-        let modalTitle= $(".modal-title");
+        let modalTitle = $(".modal-title");
         let modalHeader = $(".modal-header");
-        let modalContainer = document.querySelector('#modal-container');  
-        
+        let modalContainer = document.querySelector('#modal-container');
+
         modalTitle.empty();
         modalBody.empty();
 
-        let nameElement = $("<h1>"+ pokemon.name + "</h1>");
+        let nameElement = $("<h1>" + pokemon.name + "</h1>");
 
         let imageElementFront = $('<img class="modal-img" style="width:50%">');
         imageElementFront.attr("src", pokemon.frontSprite);
@@ -94,70 +99,70 @@ let pokemonRepository = (function () {
         modalBody.append(imageElementBack);
         modalBody.append(heightElement);
         modalBody.append(typesElement);
-      
+
         //Clear all existing modal content
         modalContainer.innerHTML = '';
-      
+
         let modal = document.createElement('div');
         modal.classList.add('modal');
-      
+
         //Add the new modal content
         let closeButtonElement = document.createElement('button');
         closeButtonElement.classList.add('modal-close');
         closeButtonElement.innerText = 'Close';
         closeButtonElement.addEventListener('click', hideModal);
-      
+
         let titleElement = document.createElement('h1');
         titleElement.innerText = pokemon.name;
-      
-        let contentElement=document.createElement('img');
+
+        let contentElement = document.createElement('img');
         contentElement.src = pokemon.frontSprite;
         contentElement.src = pokemon.backSprite;
-        
+
         modal.appendChild(closeButtonElement);
         modal.appendChild(titleElement);
         modal.appendChild(contentElement);
         modalContainer.appendChild(modal);
-      
-        
+
+
         modalContainer.classList.add('is-visible');
-      
+
         modalContainer.addEventListener('click', (e) => {
-                     //Since this is also triggered when clicking inside the modal we only want to close if the user clicks directly on the overlay
-          let target = e.target;
-          if (target === modalContainer){
-            hideModal();
-          }
+            //Since this is also triggered when clicking inside the modal we only want to close if the user clicks directly on the overlay
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
         });
-      }
-      
-      document.querySelector('#show-modal').addEventListener('click', () => {
+    }
+
+    document.querySelector('#show-modal').addEventListener('click', () => {
         showModal(pokemon);
-      });
-      
-      function hideModal(){
+    });
+
+    function hideModal() {
         document.querySelector('#modal-container').classList.remove('is-visible');
-      }
-      
-      window.addEventListener('keydown', (e) => {
+    }
+
+    window.addEventListener('keydown', (e) => {
         let modalContainer = document.querySelector('#modal-container');
-        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
             hideModal();
         }
-      });
+    });
 
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
         showDetails: showDetails,
-        LoadList : LoadList,
-        loadDetails : loadDetails
+        LoadList: LoadList,
+        loadDetails: loadDetails
     }
 })();
 
-pokemonRepository.LoadList().then(function(){
-    pokemonRepository.getAll().forEach(function(pokemon){
+pokemonRepository.LoadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
 });
